76e3e0cd48a31f5eb026b8294b1b00115393f0d3
gabearod2 <gabearod2@gmail.com>
2024-08-08 19:29:48 -0400

    Adding comments for clarity

--- src/unitree_ros2_python/unitree_ros2_python/go2_rl_actions_onnx.py
+++ src/unitree_ros2_python/unitree_ros2_python/go2_rl_actions_onnx.py
@@ -70,7 +70,7 @@ class Go2_RL_Actions(Node):
 
         # Finding and loading the ONNX model - can be changed to desired model
         share_dir = get_package_share_directory('unitree_ros2_python')
-        # pronking gait
+        # pronking gait - do not use v3 or v5 currently
         model_path = os.path.join(share_dir, 'models', 'flat_policy_v5.onnx') 
         self.get_logger().info(f"Model path: {model_path}")
         self.load_onnx_model(model_path)
