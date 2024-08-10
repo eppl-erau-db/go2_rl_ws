fc1e29f4329dcc61c28ed838a3b57e6cee019f05
gabearod2 <gabearod2@gmail.com>
2024-08-10 11:35:51 -0400

    Adding mode switching functionality

--- src/go2_sdk_integration/src/go2_ai_switcher.cpp
+++ src/go2_sdk_integration/src/go2_ai_switcher.cpp
@@ -1,25 +1,82 @@
 #include "rclcpp/rclcpp.hpp"
 #include <cstdlib>
 
+#include "unitree_go/msg/sport_mode_state.hpp"
+#include "unitree_api/msg/request.hpp"
+#include "common/ros2_sport_client.h"
+
+constexpr double sitting_limit = 0.10; // (meters)
+
 class AISwitcherNode : public rclcpp::Node
 {
 public:
-    AISwitcherNode() : Node("ai_switcher_node")
+    AISwitcherNode() 
+    : Node("ai_switcher_node"),
+    is_sitting_(false),
+    called_switcher_(false)
     {
+        // subscribing to the "sportmodestate" topic
+        state_suber = this->create_subscription<unitree_go::msg::SportModeState>(
+            "lf/sportmodestate", 10, std::bind(&AISwitcherNode::state_callback, this, std::placeholders::_1));
+
+        // sport mode request publisher and timer
+        req_puber = this->create_publisher<unitree_api::msg::Request>("/api/sport/request", 10);
+
+        // network name parameter
         this->declare_parameter<std::string>("network_interface", "enp114s0");
+        network_interface_ = this->get_parameter("network_interface").as_string();
+    }
 
-        std::string network_interface;
-        this->get_parameter("network_interface", network_interface);
+private:
 
-        std::string command = "install/go2_sdk_integration/lib/go2_sdk_integration/ai_switcher " + network_interface;
-
+    void switch_to_AI()
+    {
+        // creating, logging and executing command
+        std::string command = "install/go2_sdk_integration/lib/go2_sdk_integration/ai_switcher " + network_interface_;
         RCLCPP_INFO(this->get_logger(), "Executing: %s", command.c_str());
         int result = std::system(command.c_str());
 
+        // handling and reporting error message
         if (result != 0) {
             RCLCPP_ERROR(this->get_logger(), "Failed to execute ai_switcher with error code: %d", result);
         }
     }
+    void state_callback(unitree_go::msg::SportModeState::SharedPtr data)
+    {
+        if (data->position[2] < sitting_limit)
+        {
+            is_sitting_ = true;
+            RCLCPP_INFO(this->get_logger(), "The dog is sat down!");
+        }
+        else if (!called_switcher_)
+        {
+            sport_req.StandDown(req); // Call StandDown without assignment
+            req_puber->publish(req);
+            RCLCPP_INFO(this->get_logger(), "Sending StandDown request...");
+            return;
+        }
+
+        if (is_sitting_ && !called_switcher_)
+        {
+            switch_to_AI();
+            RCLCPP_INFO(this->get_logger(), "Calling AI switcher function!");
+            called_switcher_ = true;
+        }
+    }
+
+    // Publishers and Subscriptions 
+    rclcpp::Subscription<unitree_go::msg::SportModeState>::SharedPtr state_suber;
+    rclcpp::Publisher<unitree_api::msg::Request>::SharedPtr req_puber;
+
+    // Unitree Go ROS2 request message and sport client:
+    unitree_api::msg::Request req; 
+    SportClient sport_req;
+
+    // Position tracking
+    bool is_sitting_;
+    bool called_switcher_;
+
+    std::string network_interface_;
 };
 
 int main(int argc, char **argv)
