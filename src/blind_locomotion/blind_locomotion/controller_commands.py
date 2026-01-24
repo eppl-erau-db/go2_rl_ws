0e38943a5bdf0b5807b18ac2789cfb9dc7c393e7
Gabriel Rodriguez <gabearod2@gmail.com>
2026-01-24 09:33:02 -0500

    adding custom message type in blind_locomotion

--- src/blind_locomotion/blind_locomotion/controller_commands.py
+++ src/blind_locomotion/blind_locomotion/controller_commands.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.duration import Duration
 from geometry_msgs.msg import Twist
 from unitree_go.msg import WirelessController
-from rclpy.duration import Duration
-from std_msgs.msg import Float32MultiArray
+from blind_locomotion.msg import Button
 
 
 class WirelessControl(Node):
     def __init__(self):
         super().__init__('wireless_control')
+
+        # publishers and messages
         self.vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.buttons_publisher = self.create_publisher(
-            Float32MultiArray,
+            Button,
             'buttons',
             10)
         self.subscription = self.create_subscription(
@@ -20,15 +22,9 @@ class WirelessControl(Node):
             '/wirelesscontroller',
             self.wireless_controller_callback,
             10)
-        self.twist = Twist()
-        self.up = 0.0  # UP button state
-        self.down = 0.0  # DOWN button state
-        self.start = 0.0  # START button state
-        self.select = 0.0  # SELECT button state
-        self.a = 0.0  # A button state
-        self.b = 0.0  # B button
+
         self.speed = 1.0  # Max lin speed, m/s
-        self.turn = -1.0  # Max ang speed, rad/s (sign convention switch)
+        self.ang_speed = -1.0  # Max ang speed, rad/s (sign convention switch)
         self.last_msg_time = self.get_clock().now()
         self.timeout_duration = Duration(seconds=0.5)  # Timeout duration
         self.get_logger().info(
@@ -38,34 +34,24 @@ class WirelessControl(Node):
         self.timer = self.create_timer(0.1, self.check_timeout)
 
     def wireless_controller_callback(self, msg):
+        # publish twist message 
         self.twist = Twist()
         self.twist.linear.x = msg.ly * self.speed
-        self.twist.linear.y = msg.lx * -self.speed  # (sign convention switch)
-        self.twist.angular.z = msg.rx * self.turn
-
-        self.up = 1.0 if msg.keys == 4096 else 0.0
-        self.down = 1.0 if msg.keys == 16384 else 0.0
-        self.start = 1.0 if msg.keys == 4 else 0.0
-        self.select = 1.0 if msg.keys == 8 else 0.0
-        self.a = 1.0 if msg.keys == 256 else 0.0
-        self.b = 1.0 if msg.keys == 512 else 0.0
-
-        # Publishing the array of button states
-        button_msg = Float32MultiArray()
-        button_msg.data = [self.up, 
-                           self.down, 
-                           self.start,
-                           self.select,
-                           self.a,
-                           self.b]
-
-        # Publish the Twist message
+        self.twist.linear.y = msg.lx * -self.speed
+        self.twist.angular.z = msg.rx * self.ang_speed
         self.vel_publisher.publish(self.twist)
 
-        # Publish the buttons message
-        self.buttons_publisher.publish(button_msg)
+        # publish button message
+        self.button = Button()
+        self.button.up = True if msg.keys == 4096 else False
+        self.button.down = True if msg.keys == 16384 else False
+        self.button.start = True if msg.keys == 4 else False
+        self.button.select = True if msg.keys == 8 else False
+        self.button.a = True if msg.keys == 256 else False
+        self.button.b = True if msg.keys == 512 else False
+        self.buttons_publisher.publish(self.button)
 
-        # Update the last message time
+        # update last message time
         self.last_msg_time = self.get_clock().now()
 
     def check_timeout(self):
@@ -74,9 +60,11 @@ class WirelessControl(Node):
             zero_twist = Twist()
             self.vel_publisher.publish(zero_twist)
 
-            # Publish zero button states
-            zero_button_msg = Float32MultiArray()
-            zero_button_msg.data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+            # Publish zero button states if timeout occurs
+            zero_button_msg = Button()
+            for field_name, field_type in zero_button_msg.get_fields_and_field_types().items():
+                if field_type == 'boolean':
+                    setattr(zero_button_msg, field_name, False)
             self.buttons_publisher.publish(zero_button_msg)
 
 
