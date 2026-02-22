387e84bfeeaadb97df13f36267a9047b759e5bbf
Gabriel Rodriguez <gabearod2@gmail.com>
2026-02-22 14:09:43 -0500

    adding posture monitor class

--- src/rl_deploy/include/rl_deploy/posture_monitor.hpp
+++ src/rl_deploy/include/rl_deploy/posture_monitor.hpp
@@ -1,18 +1,30 @@
-/**
- * @file posture_monitor.hpp
- * @brief 
- * @author Gabriel Rodriguez
- */
-
 #pragma once
+#include <rclcpp/rclcpp.hpp>
 #include "unitree_go/msg/low_state.hpp"
+#include "rl_deploy/types.hpp"
+
 
 namespace rl_deploy {
 
-struct PoseQuality { 
-    bool good_stand{false}; 
-    bool good_sit{false}; 
+struct PoseQuality {
+  bool good_stand{false};
+  bool good_sit{false};
 };
 
-PoseQuality evaluate_pose(const unitree_go::msg::LowState& state);
-} // namespace rl_deploy
+struct JointLimitReport {
+  bool hard_violation{false};
+  bool all_in_soft_zone{true};
+};
+
+class PostureMonitor {
+public:
+  explicit PostureMonitor(const rclcpp::Logger& logger);
+
+  PoseQuality evaluate_pose(const unitree_go::msg::LowState& state) const;
+  JointLimitReport check_joint_limits(const unitree_go::msg::LowState& state) const;
+
+private:
+  rclcpp::Logger logger_;
+};
+
+}
